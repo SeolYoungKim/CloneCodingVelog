675ccbdad3f994fc208e728574fd64ba31b15f2a
SeolYoungKim <nasur4da@gmail.com>
2022-07-04 21:19:50 +0900

    Project init

---
+++ src/main/java/ToyProject/CloneCodingVelog/CloneCodingVelogApplication.java
@@ -0,0 +1,13 @@
+package ToyProject.CloneCodingVelog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloneCodingVelogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloneCodingVelogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ToyProject/CloneCodingVelog/CloneCodingVelogApplicationTests.java
@@ -0,0 +1,13 @@
+package ToyProject.CloneCodingVelog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloneCodingVelogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
